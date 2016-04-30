e7d49909e51d7ae7d2399a961196dfd823aee736
Rahul Bhatt <rahul444@hotmail.com>
2016-04-30 14:00:50 -0700

    basic bounce

--- ball.js
+++ ball.js
@@ -1,6 +1,6 @@
 var w = getWidth();
 var h = getHeight();
-var speed = 40;
+var speed = 10;
 var context;
 
 function ball(text, user, likes) {
@@ -18,27 +18,27 @@ function ball(text, user, likes) {
         this.time += 1;
     }
 
-    this.x = Math.floor(Math.random() * w);
-    this.y = Math.floor(Math.random() * h);
+    this.x = Math.floor(Math.random() * (w - (2 * likes)) + likes);
+    this.y = Math.floor(Math.random() * (h - (2 * likes)) + likes);
 
     var randX = Math.floor(Math.random() * 2);
     var randY = Math.floor(Math.random() * 2);
-    if (randX == 1) this.dx = -3;
-    else this.dx = 3;
-    if (randY == 1) this.dy = -3;
-    else this.dy = 3;
+    if (randX == 1) this.dx = -1.5;
+    else this.dx = 1.5;
+    if (randY == 1) this.dy = -1.5;
+    else this.dy = 1.5;
 }
 
 function init() {
   context= myCanvas.getContext('2d');
-  drawMultiple(14);
+  drawMultiple(3);
 }
 
 function drawMultiple(n) {
     var arr = [];
-    var bal = new ball("random txt", "naga", Math.floor(Math.random() * 100));
+    var bal = new ball("random txt", "naga", Math.floor(Math.random() * 100) + 50);
     for (var i = 0; i < n; i++) {
-        var bal = new ball("random txt", "naga", Math.floor(Math.random() * 100));
+        var bal = new ball("random txt", "naga", Math.floor(Math.random() * 100) + 50);
         arr.push(bal);
     }
     setInterval(draw,speed,arr);
@@ -54,14 +54,40 @@ function draw(arr) {
       // b.likes += 0.1;
       context.closePath();
       context.fill();
+      bounce(arr, i);
 
       // Boundary
-      if(b.x<0 || b.x>w) b.dx=-b.dx;
-      if(b.y<0 || b.y>h) b.dy=-b.dy;
+      if(b.x + b.dx < b.likes || b.x + b.dx > w - b.likes) b.dx = -b.dx;
+      if(b.y + b.dx < b.likes || b.y + b.dy > h - b.likes) b.dy = -b.dy;
 
       b.x+=b.dx;
       b.y+=b.dy;
   }
+}
+
+function bounce(arr, i) {
+    var b1 = arr[i];
+    for (var j = 0; j < arr.length; j++) {
+        if (i != j) {
+            var b2 = arr[j];
+            intersect(b1, b2);
+        }
+    }
+}
+
+function intersect(b1, b2) {
+    if (b1.x + b1.likes + b2.likes > b2.x
+        && b1.x < b2.x + b1.likes + b2.likes
+        && b1.y + b1.likes + b2.likes > b2.y
+        && b1.y < b2.y + b1.likes + b2.likes) {
+           var dist = Math.sqrt(((b1.x - b2.x) * (b1.x - b2.x)) + ((b1.y - b2.y) * (b1.y - b2.y)));
+           if (dist < b1.likes + b2.likes) {
+               b1.dx = -b1.dx;
+               b2.dx = -b2.dx;
+               b1.dy = -b1.dy;
+               b2.dy = -b2.dy;
+           }
+    }
 }
 
 function getWidth() {
@@ -91,3 +117,6 @@ function getHeight() {
     return document.body.clientHeight;
   }
 }
+
+// Ball sizes
+// size is fixed on creation
